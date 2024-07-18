7699d5eaf1f454a5a9c7ede1ec12dd9eb79ac601
Elena-1998 <ormomax@gmail.com>
2024-07-18 12:44:59 +0800

    0718

---
+++ script.js
@@ -0,0 +1,18 @@
+// const myCursor = document.createElement("div");
+//         myCursor.id = "myCursor";
+//         document.body.appendChild(myCursor);
+        
+//         let cursor = document.getElementById("myCursor");
+//         window.addEventListener("mousemove", function(e) {
+//             let x = e.pageX;
+//             let y = e.pageY;
+//             cursor.style.left = x - 10 + "px"; 
+//             cursor.style.top = y - 2 + "px";
+//         });
+//         window.addEventListener("click", function(e) {
+//             myCursor.className = "click"
+//             setTimeout(function() {
+//                 myCursor.className = "idle";
+//         }, 100); // 1000 毫秒 = 1 秒
+//         });
+       
